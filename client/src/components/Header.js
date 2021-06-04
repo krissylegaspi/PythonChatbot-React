455512af8f778d617cc9da6102831e01d74537dc
Kristine Legaspi <krissylegaspi@gmail.com>
2021-06-03 21:37:10 -0700

    Moved Menu to Landing Page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => (
         <div className="nav-wrapper pink lighten-2">
             <Link to={'/'} className="brand-logo center">Kristine's Coffee Shop</Link>
             <ul id="nav-mobile" className="left hide-on-med-and-down pink lighten-2">
-                <li><Link to={'/menu'}>Menu</Link></li>
+                {/* <li><Link to={'/menu'}>Menu</Link></li> */}
                 <li><Link to={'/about'}>About</Link></li>
             </ul>
         </div>
--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Landing.css';
+import ItemList from './shop/ItemList';
 
 const Landing = () => (
     <div class="landing-page" style={{ textAlign: 'center' }}>
-        <h3>Welcome!</h3>
-        Guided orders with the help of the chatbot
+        <h3>Menu</h3>
+        <ItemList />
     </div>
 )
 
